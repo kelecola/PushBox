be994fb1b49cfc41d472e3fc4daea8bd9863fcbc
kelecole <1569860141@qq.com>
2018-07-01 16:38:32 +0800

    settings修改项

---
+++ src/resume/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="header">
+            <div class="photo"></div>
+            <div class="name"></div>
+        </div>
+        <div class="body">
+            <div class="info"></div>
+            <div class="pride"></div>
+            <div class="skill"></div>
+            <div class="self"></div>
+            <div class="experience"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header{}
+</style>

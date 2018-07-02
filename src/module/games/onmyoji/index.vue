3c5953d1ea11c8ea35bd25c7d382394b2912c817
kelecole <1569860141@qq.com>
2018-07-02 22:27:59 +0800

    login_page

---
+++ src/module/games/onmyoji/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>阴阳师</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/resume/index.vue
+++ src/resume/index.vue
@@ -1,25 +1,51 @@
 <template>
-    <div>
-        <div class="header">
-            <div class="photo"></div>
-            <div class="name"></div>
+    <div class="login__wrapper">
+        <div class="username__wrapper">
+            <div class="username">用户名</div>
+            <el-input v-model="name" style=" width: 200px"></el-input>
         </div>
-        <div class="body">
-            <div class="info"></div>
-            <div class="pride"></div>
-            <div class="skill"></div>
-            <div class="self"></div>
-            <div class="experience"></div>
+        <div class="password__wrapper">
+            <div class="password">密码</div>
+            <el-input v-model="password" style=" width: 200px"></el-input>
+        </div>
+        <div>
+            <el-button type="submit" @click="check">登录</el-button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    check() {
+      if (this.name === '') {
+        alert('账号不能为空')
+      } else {
+        this.$router.push({path: '/content', query: {name: this.name, password: this.password}})
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.header{}
+.login__wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end
+}
+.username__wrapper,.password__wrapper{
+    display: flex;
+    align-items: center;
+    margin: 20px;
+}
+.username,.password{
+    margin-right: 20px;
+}
 </style>

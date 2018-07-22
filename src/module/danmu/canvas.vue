aefc6bc496bf1803f57283eb4ee9775326a7e5bb
kelecole <1569860141@qq.com>
2018-07-22 15:23:07 +0800

    7.22

---
+++ src/module/danmu/canvas.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="canvas"></div>
+</template>
+
+<script>
+// export default {
+// //   mounted() {
+// //     let canvas = document.getElementById('canvas')
+// //     let ctx = canvas.getContent('2d')
+// //     ctx.font = '20px Microsoft YaHei'
+// //     ctx.fillStyle = '#000000'
+// //     ctx.fillText('canvas 绘制文字', x, y)
+// //     ctx.clearRect(0, 0, width, height)
+// //     let colorArr = _this.getColor(color)
+// //     let numArrL = _this.getLeft()
+// //     let numArrT = _this.getTop()
+// //     let speedArr = _this.getSpeed()
+
+// //     _this.timer = setInterval(function() {
+// //       ctx.clearRect(0, 0, canvas.width, canvas.height)
+// //       ctx.save()
+// //       for (let j = 0; j < barrageList.length; j++) {
+// //         numArrL[j] -= speedArr[j]
+// //         ctx.fillStyle = colorArr[j]
+// //         ctx.fillText(barrageList[j], numArrL[j], numArrT[j])
+// //         ctx.restore()
+// //       }
+// //     }, 16.7)
+// //   }
+// // }
+</script>
+
+<style>
+
+</style>
